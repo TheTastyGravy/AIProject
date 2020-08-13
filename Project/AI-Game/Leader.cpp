f3bc36f2b291e5d996b91a0e34cef91d9074cf43
TheTastyGravy <cnpowerman23@gmail.com>
2020-08-13 11:29:25 +1000

    Add basic functionality to Leader
    Up to this point, many things were being slowed down due to the fact
    that the leader was empty. Now that it has most of what it will need
    going forward, the Swarmer class can be largely finished now.
    
    The main features are the swarm and formation vectors with getters, join
    and leave swarm functions, and the isRecruiting flag.

--- Project/AI-Game/Leader.cpp
+++ Project/AI-Game/Leader.cpp
@@ -3,6 +3,16 @@
 #include "raylib.h"
 
 
+Leader::Leader(Vector2 position, float importance) :
+	Agent(position),
+	swarm(),
+	formation(),
+	isRecruiting(false),
+	importance(importance),
+	rotation(0.0f)
+{
+}
+
 Leader::~Leader()
 {
 }
@@ -11,10 +21,49 @@ Leader::~Leader()
 void Leader::update(float deltaTime)
 {
 	//TODO: leader logic
+
+	//state machine in agent::update
+	Agent::update(deltaTime);
+
+
+	// Increment rotation
+	rotation += deltaTime * rotationSpeed;
+	if (rotation > 360)
+		rotation -= 360;
 }
 
 void Leader::draw()
 {
-	//TODO: draw leader
-	Agent::draw();
+	// Draw leaders as a rotating violet triangle
+	DrawPoly(position, 3, 8, rotation, VIOLET);
+}
+
+
+void Leader::joinSwarm(Swarmer* swarmer)
+{
+	// Add the swarmer to the swarm
+	swarm.push_back(swarmer);
+}
+
+void Leader::leaveSwarm(Swarmer* swarmer)
+{
+	// Find and remove the swarmer from the swarm
+	for (int i = 0; i < swarm.size(); i++)
+	{
+		if (swarm[i] == swarmer)
+		{
+			swarm.erase(swarm.begin() + i);
+			break;
+		}
+	}
+
+	// It could also be in formation...
+	for (int i = 0; i < formation.size(); i++)
+	{
+		if (formation[i] == swarmer)
+		{
+			formation.erase(formation.begin() + i);
+			break;
+		}
+	}
 }
--- Project/AI-Game/Leader.h
+++ Project/AI-Game/Leader.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Agent.h"
+#include "Swarmer.h"
 
 
 // An AI Agent that controls Swarmers and pathfinds between the player and spawners
@@ -7,27 +8,40 @@ class Leader :
 	public Agent
 {
 public:
-	Leader(Vector2 position);
+	Leader(Vector2 position, float importance);
 	virtual ~Leader();
 
 
 	virtual void update(float deltaTime);
 	virtual void draw();
 
-	bool getRecruiting() { return isRecruiting; }
+	void joinSwarm(Swarmer* swarmer);
+	void leaveSwarm(Swarmer* swarmer);
 
 
+	float getImportance() const { return importance; }
+
+	bool getRecruiting() const { return isRecruiting; }
+	bool setRecruiting(bool isRecruiting) { this->isRecruiting = isRecruiting; }
+
+	// Returns a reference to this leaders swarm
+	std::vector<Swarmer*>& getSwarm() { return swarm; }
+	int getSwarmSize() const { return swarm.size(); }
+
+	// Returns a reference to this leaders formation members
+	std::vector<Swarmer*>& getFormation() { return formation; }
 
 protected:
-
-
-	bool isRecruiting;// = false
-
-
-
+	std::vector<Swarmer*> swarm;
+	std::vector<Swarmer*> formation;
 
 private:
+	// When set to true and in range of swarmers, they will join this leaders swarm
+	bool isRecruiting;
+	// Used to impact other boids
+	float importance;
 
-
-
+	// Used for drawing
+	float rotation;
+	const float rotationSpeed = 360.0f;
 };
